927fc7386dd6e30838c8410c3254743fd0ddee87
Lee Jaeheon <ti6419@gmail.com>
2023-12-04 00:02:42 +0900

    @BeforeEach에서 statics 초기화 수행

--- src/test/java/com/violetbeach/hibernateplayground/support/QueryCountUtil.java
+++ src/test/java/com/violetbeach/hibernateplayground/support/QueryCountUtil.java
@@ -5,7 +5,13 @@ import jakarta.persistence.EntityManager;
 public class QueryCountUtil {
 
     public static long getSelectQueryCount(EntityManager entityManager) {
-        return entityManager.unwrap(org.hibernate.Session.class).getSessionFactory().getStatistics().getEntityLoadCount();
+        return entityManager.unwrap(org.hibernate.Session.class).getSessionFactory().getStatistics()
+            .getEntityLoadCount();
+    }
+
+    public static void clearAllCount(EntityManager entityManager) {
+        entityManager.unwrap(org.hibernate.Session.class).getSessionFactory().getStatistics()
+            .clear();
     }
 
 }
--- src/test/java/com/violetbeach/hibernateplayground/support/context/JpaTestContext.java
+++ src/test/java/com/violetbeach/hibernateplayground/support/context/JpaTestContext.java
@@ -1,11 +1,18 @@
 package com.violetbeach.hibernateplayground.support.context;
 
+import com.violetbeach.hibernateplayground.support.QueryCountUtil;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
 public class JpaTestContext {
     @PersistenceContext
     protected EntityManager em;
+
+    @BeforeEach
+    void setup() {
+        QueryCountUtil.clearAllCount(em);
+    }
 }
