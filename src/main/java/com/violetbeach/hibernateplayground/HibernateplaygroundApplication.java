cc79683f509a4548c6d5332136e6f1f5fd1aea57
Lee Jaeheon <ti6419@gmail.com>
2023-12-03 22:32:20 +0900

    chore: 프로젝트 업로드

---
+++ src/main/java/com/violetbeach/hibernateplayground/HibernateplaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.violetbeach.hibernateplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateplaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateplaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/violetbeach/hibernateplayground/HibernateplaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.violetbeach.hibernateplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateplaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
